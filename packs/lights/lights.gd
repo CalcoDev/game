31465903861a229d8db0d1c0f541006d5d6154e3
Calco Dev <calcopoddev@gmail.com>
2024-08-26 15:52:08 +0300

    feat: day and night cycle visualisation w/ shadows

--- packs/lights/lights.gd
+++ packs/lights/lights.gd
@@ -4,7 +4,20 @@ extends Node
 # const EMITTER_GROUP: StringName = &"lights_emitter"
 const OCCLUDER_GROUP: StringName = &"lights_occluder"
 
-@export var _dbg_display_tex: TextureRect = null
+@export var sky_modulate: CanvasModulate = null
+# @export var sky_day_col: Color = Color.WHITE
+# @export var sky_night_col: Color = Color.hex(0x40436e)
+
+@export var sky_colors: Gradient
+# @export var sky_morning_col: Color = Color.WHITE
+# @export var sky_midday_col: Color = Color.WHITE
+# @export var sky_sunset_col: Color = Color.WHITE
+# @export var sky_night_col: Color = Color.WHITE
+# @export var sky_midnight_col: Color = Color.WHITE
+# @export var sky_dawn_col: Color = Color.WHITE
+
+# Set => outputs to custom thing. By default should be used as a get.
+@export var output_texture: TextureRect = null
 
 @export var time_scale: float = 1.0
 @export var min_shadow_length: float = 10
@@ -37,28 +50,35 @@ func _enter_tree() -> void:
 var time_of_day: float = 0.0
 var sanglesign = 1.0
 func _process(delta: float) -> void:
-	# sun_angle += time_scale * PI * 0.5 * delta * sanglesign
-	# if sun_angle >= PI:
-	# 	sun_angle -= PI
-	# var shadow_dir = Vector2(cos(sun_angle), -sin(sun_angle)).normalized()
-	# var t = abs(cos(max(0.3, abs( shadow_dir.dot(Vector2.UP)) ) )) * 1.75 - 0.5
-	# var a1 = (sun_angle / PI)
-	# var a2 = a1 - floor(a1) * PI
-	# var a = abs((1.0 * float(a2 > 0.5)) - a2)
-	# print(a)
-	# var t = lerp(1.0, 0.2, a)
-	# var shadow_length = lerp(min_shadow_length, max_shadow_length, clamp(t, 0.0, 1.0))
-	# $"RenderPasses/ShadowPixelsPass/TextureRect".material.set_shader_parameter(&"u_light_dir", shadow_dir)
-	# $"RenderPasses/ShadowPixelsPass/TextureRect".material.set_shader_parameter(&"u_distance", shadow_length)
-	
-	# sun_angle += time_scale * PI * 0.5 * delta * sanglesign
-
 	time_of_day += time_scale * delta * 0.05
 	if time_of_day > 2.0:
 		time_of_day = 0.0
 	
-	sun_angle = time_of_day * PI
-	var shadow_dir = Vector2(cos(sun_angle), -sin(sun_angle)).normalized()
+	# # Handle time of day colours:
+	sky_modulate.color = sky_colors.sample(time_of_day / 2.0)
+	# var a: Color
+	# var b: Color
+	# var t: float = 0.0
+	# if time_of_day < 0.5: # 0 - 0.5 -> 0 - 1
+	# 	a = sky_morning_col
+	# 	b = sky_midday_col
+	# 	t = time_of_day * 2.0
+	# elif time_of_day < 0.85: # 0.5 - 0.85 -> 0 - 1
+	# 	a = sky_midday_col
+	# 	b = sky_sunset_col
+	# 	t = (time_of_day - 0.5) / 0.35
+	# elif time_of_day < 1.0:
+	# 	a = sky_sunset_col
+	# 	b = sky_night_col
+	# 	t = 
+	
+	var t = time_of_day
+	if t > 1.0:
+		t = 2.0 - time_of_day
+	sun_angle = t * PI
+	var shadow_dir = Vector2(sin(sun_angle), cos(sun_angle)).normalized()
+	if time_of_day > 1.0:
+		shadow_dir.x *= -1.0
 	var elevation = 1.0 - max(sin(sun_angle), 0.0)
 	var shadow_length = lerp(min_shadow_length, max_shadow_length, elevation)
 	$"RenderPasses/ShadowPixelsPass/TextureRect".material.set_shader_parameter(&"u_light_dir", shadow_dir)
@@ -80,8 +100,8 @@ func _setup_render_passes() -> void:
 	# shadow_pixels_mat.set_shader_parameter(&"u_step_size", Vector2(0.003125, 0.005555))
 	shadow_pixels_mat.set_shader_parameter(&"u_distance", 1.0 / 20.0)
 
-	_dbg_display_tex.size = get_viewport().get_visible_rect().size
-	_dbg_display_tex.texture = shadow_pixels.get_texture()
+	output_texture.size = get_viewport().get_visible_rect().size
+	output_texture.texture = shadow_pixels.get_texture()
 
 func _setup_scene() -> void:
 	for occluder in _get_children_with_group(get_tree().root, OCCLUDER_GROUP):
